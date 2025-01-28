9534943dc62ba3c3d8837e1e6a581ecfdbcb5c19
venoherd <3.03wave@gmail.com>
2025-01-28 22:56:15 +0100

    add bot py

---
+++ bot.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+
+API_TOKEN = '7892253798:AAFhlNyHCZDwllpzU638ssx8UYccWbIXFSE'
+dp = Dispatcher()
+
+#команда старт
+@dp.message(CommandStart())
+async def check_user_id(message: types.Message):
+    if not await users(message.from_user.id):
+        await message.reply("У вас нет доступа к этому боту.")
+        return
+    await message.reply("Добро пожаловать!")
+
+# вайтлист (база данных)
+
+ALLOWED_USERS = [431862198, 5570684597]
+#отвечает за запуск бота
+
+
+#функия доступа к базе данных
+async def users(id):
+    return id in [431862198, 5570684597]
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=API_TOKEN)
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
